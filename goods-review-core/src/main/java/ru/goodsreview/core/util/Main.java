4a2070c314694742f18217ee5b73f8b7a67ebfe9
batya <batya239@gmail.com>
2012-06-18 00:08:19 +0400

    some

---
+++ goods-review-core/src/main/java/ru/goodsreview/core/util/Main.java
@@ -0,0 +1,22 @@
+package ru.goodsreview.core.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * User: daddy-bear
+ * Date: 18.06.12
+ * Time: 0:05
+ */
+public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
+
+    public static void main(final String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("invalid arguments number");
+        }
+
+        new FileSystemXmlApplicationContext(new String[]{args[0]});
+        log.info("app started");
+    }
+}
