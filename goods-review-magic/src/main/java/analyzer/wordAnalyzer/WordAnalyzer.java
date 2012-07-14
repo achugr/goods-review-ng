c8fa5c159645fd27c217c1a6cbf79c97ad2dbbca
ilya makeev <ilya.makeev@gmail.com>
2012-07-14 16:11:45 +0400

    analyzer update

--- goods-review-magic/src/main/java/analyzer/wordAnalyzer/WordAnalyzer.java
+++ goods-review-magic/src/main/java/analyzer/wordAnalyzer/WordAnalyzer.java
@@ -1,12 +1,8 @@
 package analyzer.wordAnalyzer;
 
-import analyzer.util.sentence.PartOfSpeech;
-
-import java.io.UnsupportedEncodingException;
-
 public interface WordAnalyzer {
 
-    public PartOfSpeech partOfSpeech(String word) throws UnsupportedEncodingException;
+ //   public PartOfSpeech partOfSpeech(String word) throws UnsupportedEncodingException;
 
 //    TODO add methods, gets other useful features by word analyzer programs
 }
