ac3ef39341f73bb476b144dbeb66202931c6bf01
Artemii <dfh>
2012-07-16 21:33:35 +0400

    some changes in miner module

--- goods-review-miner/src/main/java/ru/goodsreview/api/grabber/batch/GrabberBatch.java
+++ goods-review-miner/src/main/java/ru/goodsreview/api/grabber/batch/GrabberBatch.java
@@ -3,6 +3,7 @@ package ru.goodsreview.api.grabber.batch;
 import com.google.gson.JsonObject;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Required;
 import ru.goodsreview.core.db.entity.EntityService;
 import ru.goodsreview.core.util.Batch;
 
@@ -13,26 +14,22 @@ import java.util.List;
  * 13.07.12
  */
 
-public class GrabberBatch<T> extends Batch <T>{
+public class GrabberBatch extends Batch <JSONObject>{
 
     private final static Logger log = Logger.getLogger(GrabberBatch.class);
 
-//    TODO честно говоря, пока что я не вижу смысла наследоваться от Batch, тк вот такой метод
+    private EntityService entityService;
+
+    @Required
+    public void setEntityService(EntityService entityService) {
+        this.entityService = entityService;
+    }
+
+    //    TODO честно говоря, пока что я не вижу смысла наследоваться от Batch, тк вот такой метод
 //    можно написать и в самом классе Batch и не переопределять его
 //    поскольку стратегию обновления все равно на EntityService возлагаем
     @Override
-    public void handle(List<T> list) {
-        if(list.size() < 1){
-            return;
-        }
-//        TODO store list of objects in accordance with entityType and update strategy
-        EntityService entityService = new EntityService();
-        if(! (list.get(0) instanceof JSONObject)){
-            log.error("element of list must be instance of JSONObject");
-            throw new IllegalArgumentException("element of list must be instance of JSONObject");
-        }
-        @SuppressWarnings("unchecked")
-        List<JSONObject> entities = (List<JSONObject>) list;
-        entityService.writeEntities(entities);
+    public void handle(List<JSONObject> list) {
+        entityService.writeEntities(list);
     }
 }
