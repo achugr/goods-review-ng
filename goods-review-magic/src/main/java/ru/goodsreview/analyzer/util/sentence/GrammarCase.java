9ccf5401d661c964b97e21a9f462f388db6a9e9e
Artemii <dfh>
2012-07-23 00:10:13 +0400

    grammar case

--- goods-review-magic/src/main/java/ru/goodsreview/analyzer/util/sentence/GrammarCase.java
+++ goods-review-magic/src/main/java/ru/goodsreview/analyzer/util/sentence/GrammarCase.java
@@ -8,51 +8,72 @@ package ru.goodsreview.analyzer.util.sentence;
  * ilya.makeev@gmail.com
  */
 
+//так все-таки поменьше кода получается
 public enum GrammarCase {
-    IM {
-        public String toString() {
-            return "им";
-        }
-    },
-    ROD {
-        public String toString() {
-            return "род";
-        }
-    },
-    DAT {
-        public String toString() {
-            return "дат";
-        }
-    },
-    VIN {
-        public String toString() {
-            return "вин";
-        }
-    },
-    TVOR {
-        public String toString() {
-            return "твор";
-        }
-    },
-    PRED {
-        public String toString() {
-            return "пр";
-        }
-    },
-    PART {
-        public String toString() {
-            return "парт";
-        }
-    },
-    MESTN {
-        public String toString() {
-            return "местн";
-        }
-    },
-    ZVAT {
-        public String toString() {
-            return "зват";
-        }
+    IM("им"),
+    ROD("род"),
+    DAT("дат"),
+    VIN("вин"),
+    TVOR("твор"),
+    PRED("пр"),
+    PART("парт"),
+    MESTN("местн"),
+    ZVAT("зват");
+
+    private final String value;
+
+    private GrammarCase(String value){
+        this.value = value;
+    }
+
+    public String toString(){
+        return this.value;
     }
 }
+//    IM {
+//        public String toString() {
+//            return "им";
+//        }
+//    },
+//    ROD {
+//        public String toString() {
+//            return "род";
+//        }
+//    },
+//    DAT {
+//        public String toString() {
+//            return "дат";
+//        }
+//    },
+//    VIN {
+//        public String toString() {
+//            return "вин";
+//        }
+//    },
+//    TVOR {
+//        public String toString() {
+//            return "твор";
+//        }
+//    },
+//    PRED {
+//        public String toString() {
+//            return "пр";
+//        }
+//    },
+//    PART {
+//        public String toString() {
+//            return "парт";
+//        }
+//    },
+//    MESTN {
+//        public String toString() {
+//            return "местн";
+//        }
+//    },
+//    ZVAT {
+//        public String toString() {
+//            return "зват";
+//        }
+//    }
+//}
 
