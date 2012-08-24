a624dd726b3ad1f1f356faee07a354aaf53169e7
batya <batya239@gmail.com>
2012-08-24 23:08:46 +0400

    some

--- goods-review-indexer/src/main/scala/ru/goods/review/indexer/impl/CategoryIndexer.scala
+++ goods-review-indexer/src/main/scala/ru/goods/review/indexer/impl/CategoryIndexer.scala
@@ -5,6 +5,7 @@ import ru.goods.review.indexer.Indexer
 import ru.goodsreview.core.db.entity.{Visitor, EntityType, EntityService}
 import org.apache.lucene.index.{IndexWriterConfig, IndexWriter}
 import org.apache.lucene.analysis.ru.RussianAnalyzer
+import collection.JavaConverters._
 import reflect.BeanProperty
 import org.json.JSONObject
 import ru.goodsreview.core.model.impl.json.CategoryOverJson
@@ -25,19 +26,31 @@ class CategoryIndexer extends Indexer {
 
   override def index(context: Context) {
 
+    val tmpDirectory = new SimpleFSDirectory(categoryIndexPath + TmpIndexPrefixHolder.tmpPrefix)
+
     val categoryIndexWriter = new IndexWriter(
-      new SimpleFSDirectory(categoryIndexPath),
+      tmpDirectory,
       new IndexWriterConfig(Version.LUCENE_36, new RussianAnalyzer(Version.LUCENE_36))
     )
-    categoryIndexWriter.
+    categoryIndexWriter.deleteAll()
 
     entityService.visitEntities(EntityType.CATEGORY.getTypeId, new Visitor[JSONObject] {
       override def visit(jsonObject: JSONObject) {
-        val document = categoryAsDocument(new CategoryOverJson(jsonObject))
+        categoryIndexWriter.addDocument(categoryAsDocument(new CategoryOverJson(jsonObject)))
       }
     })
 
+    categoryIndexWriter.commit()
+    categoryIndexWriter.close()
 
+    val indexDirectory = new SimpleFSDirectory(categoryIndexPath)
+    tmpDirectory.listAll().asScala.toList map (f => tmpDirectory.copy(indexDirectory, f, f))
   }
+
+}
+
+private object TmpIndexPrefixHolder {
+
+  def tmpPrefix = "T_M_P"
 
 }
