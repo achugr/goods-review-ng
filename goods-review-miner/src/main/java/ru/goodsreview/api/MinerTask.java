41e64229228e7b1212454fe0349be40b492d3cd7
Timur Mokaev <timoshamokaev@gmail.com>
2012-09-29 00:34:29 +0400

    miner task starting

--- goods-review-miner/src/main/java/ru/goodsreview/api/MinerTask.java
+++ goods-review-miner/src/main/java/ru/goodsreview/api/MinerTask.java
@@ -8,33 +8,30 @@ package ru.goodsreview.api;
  * To change this template use File | Settings | File Templates.
  */
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Required;
 import ru.goodsreview.api.grabber.ReviewGrabber;
-import ru.goodsreview.core.db.entity.EntityService;
 import ru.goodsreview.scheduler.SchedulerTask;
 import ru.goodsreview.scheduler.TaskResult;
 import ru.goodsreview.scheduler.context.Context;
 
-import java.util.List;
-
 public class MinerTask implements SchedulerTask {
 
-    private EntityService entityService;
+    private ReviewGrabber reviewGrabber;
 
     @Required
-    public void setEntityService(final EntityService entityService) {
-        this.entityService = entityService;
+    public void setReviewGrabber(ReviewGrabber reviewGrabber){
+        this.reviewGrabber = reviewGrabber;
+    }
+
+    @Required
+    public ReviewGrabber getReviewGrabber(){
+        return reviewGrabber;
     }
 
     @Override
     public TaskResult run(final Context context) {
 
-        ReviewGrabber reviewGrabber = new ReviewGrabber();
-
-        List<JSONObject> reviews = reviewGrabber.grabAllReviews();
-
-        entityService.writeEntities(reviews);
+        reviewGrabber.grabAllReviews();
 
         return TaskResult.ok();
     }
