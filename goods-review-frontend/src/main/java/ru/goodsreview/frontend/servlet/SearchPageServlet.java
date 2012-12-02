0704eb031495ee89484881a8433aed7cd2378678
Artemii <achugr@yandex-team.ru>
2012-12-02 14:25:46 +0400

    some exprmnts with frontend

--- goods-review-frontend/src/main/java/ru/goodsreview/frontend/servlet/SearchPageServlet.java
+++ goods-review-frontend/src/main/java/ru/goodsreview/frontend/servlet/SearchPageServlet.java
@@ -15,10 +15,18 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/search")
 public class SearchPageServlet {
+    private final static int DEFAULT_MODELS_ON_PAGE = 9;
+
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String getSearchResults(@QueryParam("query") final String searchQuery, @QueryParam("page-number") int pageNumber, @QueryParam("models-on-page") int modelsOnPage){
+        if(pageNumber < 1){
+            pageNumber = 1;
+        }
+        if(modelsOnPage < 1){
+            modelsOnPage = DEFAULT_MODELS_ON_PAGE;
+        }
         return new SearchPageController().generatePage(searchQuery, pageNumber, modelsOnPage);
     }
 }
